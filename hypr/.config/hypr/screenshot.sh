f425559798484d82384dcbd11f251ed453809459
Danilo Patrial <danmcamposp@gmail.com>
2025-08-11 16:14:12 -0300

    remove msg from screnshot

--- hypr/.config/hypr/screenshot.sh
+++ hypr/.config/hypr/screenshot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 region=$(slurp)
 if [ -z "$region" ]; then
-  notify-send "Screenshot cancelled"
   exit 1
 fi
 
 filename=~/Pictures/screenshot-$(date +%F-%T).png
 grim -g "$region" "$filename" && wl-copy < "$filename"
-notify-send "Screenshot saved and copied"
